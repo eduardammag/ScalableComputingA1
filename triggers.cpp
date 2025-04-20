619dd9b873d96d1ed1cfc322187af1adb7c0418e
marimarifr <125320660+marimarifr@users.noreply.github.com>
2025-04-20 15:17:23 -0300

    Simulando os dados na chamada do trigger

--- triggers.cpp
+++ triggers.cpp
@@ -1,4 +1,22 @@
 #include "triggers.hpp"
+#include <iostream>
+
+void rodarScriptPython() {
+    std::string comando;
+
+#ifdef _WIN32
+    // Windows
+    comando = "python \"mock/simulator.py\"";
+#else
+    // Linux / Mac
+    comando = "python3 mock/simulator.py";
+#endif
+
+    int result = system(comando.c_str());
+    if (result != 0) {
+        std::cerr << "Erro ao executar o script Python!" << std::endl;
+    }
+}
 
 Trigger::Trigger(function<void()> func, int interval, bool timer)
     : callback(func), interval(interval), isTimer(timer), running(false) {}
@@ -14,13 +32,11 @@ void Trigger::start()
         {
             while (running) 
             {
-                // populando as bases usadas
-                system(R"( python "mock/simulator.py")");
-
+                
                 auto start_time = chrono::steady_clock::now();
                 // executa a pipeline
                 callback();
-
+                
                 auto end_time = chrono::steady_clock::now();
                 auto elapsed = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
                 auto sleep_time = chrono::milliseconds(interval) - elapsed;
@@ -30,6 +46,8 @@ void Trigger::start()
                 {
                     this_thread::sleep_for(sleep_time);
                 }
+                // populando as bases usadas
+                rodarScriptPython();
             }
         }).detach();
     } 
